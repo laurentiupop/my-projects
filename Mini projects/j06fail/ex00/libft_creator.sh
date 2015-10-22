db9903c8630c7f82640040e10661df268fbaf92c
Florin laurentiu POP <flpop@e1p80.cluj.42.fr>
2015-10-22 15:01:07 +0200

    change directory name

--- Mini projects/j06fail/ex00/libft_creator.sh
+++
@@ -1,1 +0,0 @@
-gcc -c ft_putstr.c ft_putchar.c ft_strcmp.c ft_swap.c ft_strlen.c && ar rc libft.a ft_putchar.o ft_putstr.o ft_strlen.o ft_strcmp.o ft_swap.o | ranlib libft.a 
--- Mini projects/proj00/main.c
+++
@@ -1,5 +0,0 @@
-int		main(int argc, char **argv)
-{
-		sastantua (4);
-	return (0);
-}
